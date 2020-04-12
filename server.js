2e623316c87ce40593c0de2331ae590a65599882
Tri Vo <tri.minh.vo@mgm-tp.com>
2020-04-12 21:03:58 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const mockUsers = [
   { name: 'Mark' },
@@ -21,6 +24,29 @@ app.get('/users/:id', function (req, res) {
     message: "successfully get one user",
     user: req.params.id
   });
+});
+
+const mockLoginInfo = {
+  username: 'vominhtri',
+  password: 'superSecret'
+};
+
+app.post('/login', function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === mockLoginInfo.username && password == mockLoginInfo.password) {
+    res.json({
+      success: true,
+      message: 'username and password match',
+      token: 'encripted token'
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'username and password do not match'
+    });
+  }
 });
 
 app.listen(8000, function () {
