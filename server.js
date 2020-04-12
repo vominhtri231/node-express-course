1c7018e194d6dbf4899f85cbc8caaedb80958451
Tri Vo <tri.minh.vo@mgm-tp.com>
2020-04-12 20:42:26 +0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+const mockUsers = [
+  { name: 'Mark' },
+  { name: 'Jill' }
+];
+
+app.get('/users', function (req, res) {
+  res.json({
+    success: true,
+    message: 'successfully get users',
+    users: mockUsers
+  });
+});
+
 app.listen(8000, function () {
-  console.log("server is running")
+  console.log('server is running');
 })
